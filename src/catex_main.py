4a28979239f427858b4e6666f835cbac7c52b6d9
ppiazi <ppiazi@gmail.com>
2017-03-25 16:55:56 +0900

    최초 파일 커밋
    - csv를 입력으로 받아 다수의 파일을 처리할 수 있도록 수정
    - 파일 경로, 시작지점, upper bound, lower bound 를 지정함.

---
+++ src/catex_main.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2015 Joohyun Lee(ppiazi@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import sys
+import os
+import getopt
+import csv
+import catex
+
+__version__ = 'v0.0.1'
+
+def print_usage():
+    """
+    사용법에 대한 내용을 콘솔에 출력한다.
+    :return:
+    """
+    print("catex_main.py [-f files_name]")
+    print("    Version %s" % __version__)
+    print("    Options:")
+    print("    -f : set target file")
+    print("    -o : set output file")
+
+class catex_main:
+    def __init__(self):
+        pass
+
+    def __parse(self, row):
+        in_file_start_line = 1
+        in_file_ub = 0
+        in_file_lb = 0
+
+        try:
+            temp_int = int(row[1])
+            in_file_start_line = temp_int
+        except Exception as e:
+            print(str(e))
+            in_file_start_line = 1
+
+        try:
+            temp_int = int(row[2])
+            in_file_ub = temp_int
+        except Exception as e:
+            in_file_ub = 0
+
+        try:
+            temp_int = int(row[3])
+            in_file_lb = temp_int
+        except Exception as e:
+            in_file_lb = 0
+
+        return in_file_start_line, in_file_ub, in_file_lb
+
+    def catex(self, file_name):
+        self.result_db = []
+
+        try:
+            csv_file = open(file_name, "r", encoding='utf-8')
+        except Exception as e:
+            print(str(e))
+            return
+
+        csv_reader = csv.reader(csv_file)
+
+        i = 0
+        for row in csv_reader:
+            in_file_name = ""
+            in_file_start_line = 1
+            in_file_ub = 0
+            in_file_lb = 0
+
+            try:
+                in_file_name = row[0].replace(u'\ufeff', '')
+            except Exception as e:
+                print(str(e))
+                in_file_name = row[0]
+
+            if in_file_name == "":
+                continue
+
+            in_file_start_line, in_file_ub, in_file_lb = self.__parse(row)
+
+            t_catex = catex.catex(in_file_name, in_file_start_line, in_file_ub, in_file_lb)
+            t_key = "%s : %d %d %d" % (in_file_name, in_file_start_line, in_file_ub, in_file_lb)
+
+            #print("(%d) Try to read : %s" % (i, t_key))
+            ret = t_catex.catex()
+            if ret != 0:
+                print("Error to read %s" % (t_key))
+                continue
+
+            t_result = t_catex.get_code().strip()
+
+            i = i + 1
+            t_dic = {}
+            t_dic["key"] = t_key
+            t_dic["code"] = t_result
+
+            self.result_db.append(t_dic)
+
+    def save(self, output_file_name = ""):
+        output_file_name = "output.csv"
+        try:
+            output_csv = open(output_file_name, "w")
+        except Exception as e:
+            print(str(e))
+            return 1
+
+        writer = csv.writer(output_csv)
+
+        for t_item in self.result_db:
+            t_list = []
+            t_list.append(t_item["key"])
+            t_list.append(t_item["code"].strip())
+            writer.writerow(t_list)
+
+        output_csv.close()
+
+
+if __name__ == "__main__":
+    optlist, args = getopt.getopt(sys.argv[1:], "f:o:")
+
+    file_name = ""
+    output_file_name = ""
+
+    for op, p in optlist:
+        if op == "-f":
+            file_name = p
+        elif op == "-o":
+            output_file_name = p
+        else:
+            file_name = op
+            print("Invalid Argument : %s / %s" % (op, p))
+
+    if file_name == "":
+        print_usage()
+        os._exit(1)
+
+    t_catex_main = catex_main()
+    t_catex_main.catex(file_name)
+    t_catex_main.save(output_file_name)
